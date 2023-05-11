c37e9c1dbc0d33580aa31d0ee8b1cc1167683bb7
itsparasbisht <iamparasbisht@gmail.com>
2023-05-11 12:47:45 +0530

    update order route

--- routes/order.js
+++ routes/order.js
@@ -20,4 +20,32 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.post("/update", async (req, res) => {
+  try {
+    const data = req.body;
+    const updatedOrder = await Order.findOneAndUpdate(
+      { order_id: data.order_id },
+      { $set: data },
+      { new: true }
+    );
+
+    if (updatedOrder === null) {
+      return res
+        .status(404)
+        .json({ order_id: data.order_id, message: "order_id does not exist" });
+    }
+    return res.status(200).json(updatedOrder);
+  } catch (error) {
+    const errorType = error.name;
+
+    if (errorType === "ValidationError") {
+      res.status(422).json({ message: error.message });
+    } else if (errorType === "MongoServerError") {
+      res.status(409).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: error.message });
+    }
+  }
+});
+
 module.exports = router;
